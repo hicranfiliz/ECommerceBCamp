ce9dc90af2eb9a3024ade0f86ff1bdb279271d14
hicranfiliz <h.filiz781@gmail.com>
2024-11-19 01:46:18 +0300

    created custom toolbar

---
+++ app/src/main/java/com/example/ecommercebcamp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.ecommercebcamp
+
+import android.database.DatabaseUtils
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.collection.intFloatMapOf
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.ecommercebcamp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        initUI()
+    }
+
+    private fun initUI(){
+        binding.customToolBar.imgFav.setOnClickListener {
+            Toast.makeText(this, "Fav button tapped", Toast.LENGTH_LONG).show()
+        }
+    }
+}
