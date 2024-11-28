4cf920635de0937843a03163a4265df9df5a4262
hicranfiliz <h.filiz781@gmail.com>
2024-11-28 23:15:25 +0300

    added back pressed message

--- app/src/main/java/com/example/ecommercebcamp/activities/MainActivity.kt
+++ app/src/main/java/com/example/ecommercebcamp/activities/MainActivity.kt
@@ -2,21 +2,20 @@ package com.example.ecommercebcamp.activities
 
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.databinding.DataBindingUtil
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
 import com.example.ecommercebcamp.R
 import com.example.ecommercebcamp.databinding.ActivityMainBinding
-import com.example.ecommercebcamp.databinding.FragmentHomeBinding
 
 class MainActivity : AppCompatActivity() {
 
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
+
+    private var backPressedTime = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +28,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         supportActionBar?.hide()
 
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                showExitConfirmationDialog()
+            }
+        })
     }
+
+    private fun showExitConfirmationDialog() {
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+            finish()
+        } else {
+            Toast.makeText(applicationContext, "Press back again to exit app!", Toast.LENGTH_SHORT)
+                .show()
+        }
+        backPressedTime = System.currentTimeMillis()
+    }
+
 }
